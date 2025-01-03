d9f97b3397fcd9de80706f7e7fd42f9a1a2f0d65
cbahern <99370435+cbahern@users.noreply.github.com>
2025-01-03 13:35:52 -0500

    Update Docker_build_PROD.cmd

--- Docker_build_PROD.cmd
+++ Docker_build_PROD.cmd
@@ -5,14 +5,14 @@ pause
 
 REM version
 $json = Get-Content 'package.json' | Out-String | ConvertFrom-Json
-docker build --no-cache -t "ahaen/azuread-ldap-wrapper:v$($json.version)" .
-docker buildx build --no-cache --push --platform linux/amd64,linux/arm64/v8,linux/arm/v7 -t "ahaen/azuread-ldap-wrapper:v$($json.version)" .
+docker build --no-cache -t "cbahern/azuread-ldap-wrapper:v$($json.version)" .
+docker buildx build --no-cache --push --platform linux/amd64,linux/arm64/v8,linux/arm/v7 -t "cbahern/azuread-ldap-wrapper:v$($json.version)" .
 
 echo "build latest"
 pause
 
 REM latest
-docker build --no-cache -t ahaen/azuread-ldap-wrapper:latest .
-docker buildx build --no-cache --push --platform linux/amd64,linux/arm64/v8,linux/arm/v7 -t ahaen/azuread-ldap-wrapper:latest .
+docker build --no-cache -t cbahern/azuread-ldap-wrapper:latest .
+docker buildx build --no-cache --push --platform linux/amd64,linux/arm64/v8,linux/arm/v7 -t cbahern/azuread-ldap-wrapper:latest .
 
 pause
